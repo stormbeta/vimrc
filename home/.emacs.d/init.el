dae447dd6a23ab238add40705fea717f6a784e62
Jason Miller <stormbeta@gmail.com>
2021-06-21 13:13:22 -0600

    Delete unused proton/emacs config
    If I ever end up trying emacs or proton again, I can always re-add or
    split into separate repo.

--- home/.emacs.d/init.el
+++
@@ -1,5 +0,0 @@
-(setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
-                         ("org" . "http://orgmode.org/elpa/")
-                         ("gnu" . "http://elpa.gnu.org/packages/")))
-(require 'package)
-(package-initialize)
